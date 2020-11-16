751670dd0ca7fa8a9cea5bf6bb97a6489ad0438c
met4000 <nathsharwor@hotmail.com>
2020-11-16 17:46:33 +0800

    change mouse detection and add `blocked` type

--- main.js
+++ main.js
@@ -1,38 +1,78 @@
+const ClickType = Object.freeze({
+  LEFT:   { name: "left" },
+  MIDDLE: { name: "middle" },
+  RIGHT:  { name: "right" },
+});
+var getClickTypeFromEvent = function (e) {
+  if      (e.button == 0) return ClickType.LEFT;
+  else if (e.button == 1) return ClickType.MIDDLE;
+  else if (e.button == 2) return ClickType.RIGHT;
+  
+  return undefined;
+};
+
 var rowInput  = document.getElementById("rowInput");
 var colInput  = document.getElementById("colInput");
 var table     = document.getElementById("table");
 
+var TableElemType = {
+  FILLED:   { name: "filled", getInverse: {}, getBlocked: {} },
+  EMPTY:    { name: "empty", getInverse: {}, getBlocked: {} },
+  BLOCKED:  { name: "blocked", getInverse: {}, getBlocked: {} },
+};
+TableElemType.FILLED.getInverse  = TableElemType.EMPTY;
+TableElemType.EMPTY.getInverse   = TableElemType.FILLED;
+TableElemType.BLOCKED.getInverse = TableElemType.BLOCKED;
+TableElemType.FILLED.getBlocked  = TableElemType.BLOCKED;
+TableElemType.EMPTY.getBlocked   = TableElemType.BLOCKED;
+TableElemType.BLOCKED.getBlocked = TableElemType.EMPTY;
 var _table = [];
 var _r = () => _table.length, _c = () => (_table[0] || []).length;
 
 var changeTableSize = function (r, c) {
-  while (_r() < r) _table.push(new Array(_c()).fill(false));
+  while (_r() < r) _table.push(new Array(_c()).fill(TableElemType.EMPTY));
   while (_r() > r) _table.pop();
 
-  while (_c() < c) _table.forEach(r => r.push(false));
+  while (_c() < c) _table.forEach(r => r.push(TableElemType.EMPTY));
   while (_c() > c) _table.forEach(r => r.pop());
 };
 
 var updateDisplayTable = function () {
-  table.innerHTML = _table.map((r, _r) => `<tr>${r.map((v, _c) => `<td${v ? ` class="selected"` : ``} id="[${[_r,_c]}]"></td>`).join("")}</tr>`).join("");
+  table.innerHTML = _table.map((r, _r) => `<tr>${r.map((v, _c) => `<td class="${v.name}" id="[${[_r,_c]}]"></td>`).join("")}</tr>`).join("");
   Array(...table.getElementsByTagName("td")).forEach(v => {
-    v.onclick = (...args) => clickevent(false, ...args);
-    v.oncontextmenu = (...args) => clickevent(true, ...args);
+    v.onclick = (...args) => clickevent(getClickTypeFromEvent(...args), ...args);
+    v.onauxclick = (...args) => clickevent(getClickTypeFromEvent(...args), ...args);
+    v.oncontextmenu = () => false;
   });
 };
 
-var clickevent = function (right, e) {
+var clickevent = function (clickType, e) {
   var coords = JSON.parse(e.srcElement.id);
   
-  if (right) singleChange(...coords);
-  else localChange(...coords);
-  
+  switch (clickType) {
+    case ClickType.LEFT:
+      localChange(...coords);
+      break;
+
+    case ClickType.MIDDLE:
+      block(...coords);
+      break;
+
+    case ClickType.RIGHT:
+      singleChange(...coords);
+      break;
+  }
+
   updateDisplayTable();
-  if (right) return false;
+  if (clickType == ClickType.RIGHT) return false;
 };
 
 var singleChange = function (r, c) {
-  if (0 <= r && r < _table.length) if (0 <= c && c < _table[r].length) _table[r][c] = !_table[r][c];
+  if (0 <= r && r < _table.length) {
+    if (0 <= c && c < _table[r].length) {
+      _table[r][c] = _table[r][c].getInverse;
+    }
+  }
 };
 
 var localChange = function (r, c) {
@@ -41,6 +81,14 @@ var localChange = function (r, c) {
     [  r, c-1], [  r, c  ], [  r, c+1],
                 [r+1, c  ],
   ].forEach(v => singleChange(...v));
+};
+
+var block = function (r, c) {
+  if (0 <= r && r < _table.length) {
+    if (0 <= c && c < _table[r].length) {
+      _table[r][c] = _table[r][c].getBlocked;
+    }
+  }
 };
 
 var sizeUpdate = function () {
